3f49bdb65710ecbf4d28dc59719805be9cb6fcf4
Aldairtunq <201912945b@utea.edu.pe>
2022-11-14 21:21:44 -0500

    primer commit bdSqflite

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_application_1/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TaskBD App',
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+
